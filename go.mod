b8e1f9d3cfb8d70face16769e368a709e38bab32
sh0rez <me@shorez.de>
2020-05-19 17:50:04 +0200

    feat: cli

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/sh0rez/packup
 go 1.14
 
 require (
+	github.com/go-clix/cli v0.1.2
 	github.com/gorilla/mux v1.7.4
 	github.com/markbates/pkger v0.15.1
-	gopkg.in/yaml.v2 v2.2.7
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -1,33 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-clix/cli"
 	"github.com/markbates/pkger"
+
 	"github.com/sh0rez/packup/pkg/api"
 	"github.com/sh0rez/packup/pkg/config"
 )
 
+var Version string = "dev"
+
 func main() {
-	cfg, err := config.LoadFile("packup.yaml")
-	if err != nil {
-		log.Fatalln(err)
+	cmd := &cli.Command{
+		Use:     "packup",
+		Short:   "Easy and efficient backups using Restic",
+		Version: Version,
 	}
 
-	apiHandler, err := api.New(cfg.Jobs)
-	if err != nil {
-		log.Fatalln(err)
+	configFile := cmd.Flags().String("config", "packup.yaml", "YAML config file")
+
+	cmd.Run = func(cmd *cli.Command, args []string) error {
+		cfg, err := config.LoadFile(*configFile)
+		if err != nil {
+			return err
+		}
+
+		apiHandler, err := api.New(cfg.Jobs)
+		if err != nil {
+			return err
+		}
+
+		// register api
+		http.Handle("/api/v1/", http.StripPrefix("/api/v1", apiHandler))
+
+		// register ui
+		spa := &spaFs{pkged: pkger.Dir("/ui/build")}
+		http.Handle("/", http.FileServer(spa))
+
+		log.Println("Listening on :2112")
+		if err := http.ListenAndServe(":2112", nil); err != nil {
+			return err
+		}
+
+		return nil
 	}
 
-	// register api
-	http.Handle("/api/v1/", http.StripPrefix("/api/v1", apiHandler))
-
-	// register ui
-	http.Handle("/", http.FileServer(pkger.Dir("/ui/build")))
-
-	log.Println("Listening on :2112")
-	if err := http.ListenAndServe(":2112", nil); err != nil {
-		log.Fatalln(err)
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+}
+
+// spaFs implements http.FileSystem to serve a single page application, meaning
+// it always returns `index.html`
+type spaFs struct {
+	pkged http.FileSystem
+}
+
+func (fs *spaFs) Open(name string) (http.File, error) {
+	f, err := fs.pkged.Open(name)
+	if os.IsNotExist(err) {
+		return fs.pkged.Open("/index.html")
+	}
+	return f, err
 }
