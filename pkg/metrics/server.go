4007f27673b9c641aa8430687fe74492178e1a03
sh0rez <me@shorez.de>
2020-05-21 00:01:48 +0200

    fix: async metric

--- pkg/metrics/server.go
+++ pkg/metrics/server.go
@@ -1,18 +1,48 @@
 package metrics
 
 import (
+	"sync"
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/rs/zerolog/log"
 
 	"github.com/sh0rez/packup/pkg/config"
-	"github.com/sh0rez/packup/pkg/restic"
 	"github.com/sh0rez/packup/pkg/size"
 )
 
 const Namespace = "packup"
 
 func WatchDir(name string, job config.Job) {
+	log := log.With().Str("job", name).Logger()
 	labels := prometheus.Labels{"name": name}
+
+	tick := time.NewTicker(30 * time.Second)
+	mut := &sync.Mutex{}
+
+	var sizeBytes float64
+	go func() {
+		first := true
+		for {
+			if !first {
+				_ = <-tick.C
+			}
+			first = false
+
+			n, err := size.Size(job.Repo)
+			if err != nil {
+				log.Debug().Err(err).Msg("Failed to calculate repo size")
+				continue
+			}
+
+			mut.Lock()
+			sizeBytes = float64(n)
+			mut.Unlock()
+
+			log.Debug().Float64("size", sizeBytes).Msg("Repository size calculated")
+		}
+	}()
 
 	// directory sizes
 	promauto.NewGaugeFunc(
@@ -23,46 +53,49 @@ func WatchDir(name string, job config.Job) {
 			ConstLabels: labels,
 		},
 		func() float64 {
-			n, err := size.Size(job.Repo)
-			if err != nil {
-				return -1
-			}
-			return float64(n)
+			mut.Lock()
+			defer mut.Unlock()
+
+			return float64(sizeBytes)
 		},
 	)
 
 	// snapshots count
-	rst := restic.New(job.Repo, job.Password, name)
-	promauto.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Namespace:   Namespace,
-			Name:        "snapshots_total",
-			Help:        "Count of snapshots in the repository",
-			ConstLabels: labels,
-		},
-		func() float64 {
-			s, err := rst.Snapshots()
-			if err != nil {
-				return -1
-			}
-			return float64(len(s))
-		},
-	)
+	// rst := restic.New(job.Repo, job.Password, name)
+	// promauto.NewGaugeFunc(
+	// 	prometheus.GaugeOpts{
+	// 		Namespace:   Namespace,
+	// 		Name:        "snapshots_total",
+	// 		Help:        "Count of snapshots in the repository",
+	// 		ConstLabels: labels,
+	// 	},
+	// 	func() float64 {
+	// 		t := time.Now()
+	// 		s, err := rst.Snapshots()
+	// 		if err != nil {
+	// 			return -1
+	// 		}
+	// 		log.Debug().Str("job", name).Dur("took", time.Since(t)).Msg("snapshots_total")
+	// 		return float64(len(s))
+	// 	},
+	// )
 
 	// last successful timestamp
-	promauto.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Namespace:   Namespace,
-			Name:        "last_snapshot_seconds",
-			Help:        "UNIX seconds of the last successful snapshot",
-			ConstLabels: labels,
-		},
-		func() float64 {
-			s, err := rst.Snapshots()
-			if err != nil {
-				return -1
-			}
-			return float64(s[len(s)-1].Time.Unix())
-		},
-	)
+	// promauto.NewGaugeFunc(
+	// 	prometheus.GaugeOpts{
+	// 		Namespace:   Namespace,
+	// 		Name:        "last_snapshot_seconds",
+	// 		Help:        "UNIX seconds of the last successful snapshot",
+	// 		ConstLabels: labels,
+	// 	},
+	// 	func() float64 {
+	// 		s, err := rst.Snapshots()
+	// 		t := time.Now()
+	// 		if err != nil {
+	// 			return -1
+	// 		}
+	// 		log.Debug().Str("job", name).Dur("took", time.Since(t)).Msg("last_snapshot_seconds")
+	// 		return float64(s[len(s)-1].Time.Unix())
+	// 	},
+	// )
 }
