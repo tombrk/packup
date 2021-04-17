cea48a500bffe1a5ade366ee012808859ddbd75d
sh0rez <me@shorez.de>
2021-04-17 15:01:45 +0200

    fix(api): properly handle paths when dumping

--- pkg/restic/dump.go
+++ pkg/restic/dump.go
@@ -3,10 +3,13 @@ package restic
 import (
 	"io"
 	"os"
+	"strings"
 )
 
 func (r Restic) Dump(w io.Writer, snapshot string, filepath string) error {
-	cmd := r.cmd("dump", []string{snapshot, filepath})
+	path := strings.TrimPrefix(filepath, "/")
+
+	cmd := r.cmd("dump", []string{snapshot, path})
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = w
 
